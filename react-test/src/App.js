9e565fdc62a5aff4eddd716440afe1d4e1bf652b
jaco6895@gmail.com <jisu94@antock.com>
2021-03-22 18:00:25 +0900

    할일 정리 웹앱어플리케이션 style작업 완료

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import './scss/style.scss';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 const App = () => {
-    return
-    <TodoTemplate>
-        <TodoInsert />
-    </TodoTemplate>;
+    return(
+        <TodoTemplate>
+            <TodoInsert/>
+            <TodoList/>
+        </TodoTemplate>
+    );
 };
 
 export default App;
