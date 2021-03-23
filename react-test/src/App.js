26ea523acb6b2edeff36936ff114b4e6ccfdfde1
jaco6895@gmail.com <jisu94@antock.com>
2021-03-23 18:04:51 +0900

    제작중 todoInsert 제작중

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useCallback } from "react";
 import './scss/style.scss';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
@@ -22,9 +22,27 @@ const App = () => {
             checked:false,
         }
         ]);
+    //고윳값으로 사용될 id
+    //ref를 사용하여 변수 담기
+    const nextId = useRef(4);
+
+    const onInsert = useCallback(
+        text => {
+            const todo = {
+                id: nextId.current,
+                text,
+                checked : false,
+            };
+            setTodos(todos.concat(todo));
+            nextId.current +=1; //index 1씩 더하기기
+       },
+        [todos],
+    );
+
+
     return(
         <TodoTemplate>
-            <TodoInsert/>
+            <TodoInsert onInsert={onInsert}/>
             <TodoList todos={todos}/>
         </TodoTemplate>
     );
--- react-test/src/components/TodoInsert.js
+++ react-test/src/components/TodoInsert.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React,{useState , useCallBack} from "react";
 import { MdAdd } from 'react-icons/md';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+    const [value,setValue] = useState('');
+    const onChange = useCallBack (e => {
+        setValue(e.target.value);
+    },[]);
+    const onSubmit = useCallBack( e =>{
+        onInsert(value);
+        setValue('');
+        //submit 이벤트는 브라우저에서 새로고침을 발생 이를 방지하기위해 함수 호출
+    });
     return(
         <form className="TodoInsert">
             <input placeholder="할 일을 입력하세요."/>
