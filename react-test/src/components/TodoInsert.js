ce7d033de37e112ac50f78a6ad0997e364dd3b0c
jaco6895@gmail.com <jisu94@antock.com>
2021-03-24 18:00:51 +0900

    리액트 토글 함수 작성중..... 자세히 파보기 처음보는 형식의 문법을 봣음 (...???)

--- react-test/src/components/TodoInsert.js
+++ react-test/src/components/TodoInsert.js
@@ -1,19 +1,22 @@
-import React,{useState , useCallBack} from "react";
+import React,{useState , useCallback} from "react";
 import { MdAdd } from 'react-icons/md';
 
 const TodoInsert = ({ onInsert }) => {
     const [value,setValue] = useState('');
-    const onChange = useCallBack (e => {
+
+    const onChange = useCallback (e => {
         setValue(e.target.value);
     },[]);
-    const onSubmit = useCallBack( e =>{
-        onInsert(value);
-        setValue('');
-        //submit 이벤트는 브라우저에서 새로고침을 발생 이를 방지하기위해 함수 호출
-    });
+    const onSubmit = useCallback( e =>{
+            onInsert(value);
+            setValue('');
+            //submit 이벤트는 브라우저에서 새로고침을 발생 이를 방지하기위해 함수 호출
+            e.preventDefault();
+        },[onInsert,value],
+    );
     return(
-        <form className="TodoInsert">
-            <input placeholder="할 일을 입력하세요."/>
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input placeholder="할 일을 입력하세요." value={value} onChange={onChange}/>
             <button className="btn" type="submit">
                 <MdAdd/>
             </button>
--- react-test/src/components/TodoListItem.js
+++ react-test/src/components/TodoListItem.js
@@ -4,15 +4,15 @@ import {
     MdCheckBoxOutlineBlank,MdCheckBox,MdRemoveCircleOutline
 } from "react-icons/md";
 
-const TodoListItem = ({todo}) =>{
-    const {text , checked} = todo;
+const TodoListItem = ({todo,onRemove}) =>{
+    const {id , text , checked} = todo;
     return(
         <div className="TodoListItem">
             <div className={cn('checkbox',{ checked })}>
                 {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank/>}
                 <div className="text">{text}</div>
             </div>
-            <div className="remove">
+            <div className="remove" onClick={function(){onRemove(id)}}>
                 <MdRemoveCircleOutline />
             </div>
         </div>
